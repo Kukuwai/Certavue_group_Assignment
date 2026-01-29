e642180c6e57884e10692c79cf5ff4cae146809e
ColyanM <colyanmiller@yahoo.com>
2026-01-29 21:00:51 +1300

    Worked on greedy alg some the grid is still giving me an issue and it is returning 100% to start and finish so will need some work

---
+++ CertavueApp/GreedyAlg.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GreedyAlg
+{
+    public void StartGreedy(List<Person> people, List<Project> projects)
+    {
+        //state keeps track of shifts
+        var state = new ScheduleState(people, projects);
+        BuildGreedySchedule(state);
+    }
+
+    public void BuildGreedySchedule(ScheduleState state)
+    {
+        Console.WriteLine("Greedy algorithm running");
+
+        //Harder weeks go first aka projects with most X's
+        var ordered = state.Projects
+            .OrderByDescending(p => p.people.Count * state.GetDuration(p))
+            .ToList();
+
+
+        //takes the ordered list and looks for each ones best shift
+        foreach (var project in ordered)
+        {
+            int bestShift = 0;
+            int bestConflicts = int.MaxValue;
+
+            foreach (int shift in state.GetValidShifts(project))
+            {
+                int conflicts = CountConflicts(state, project, shift);
+                if (conflicts < bestConflicts)
+                {
+                    bestConflicts = conflicts;
+                    bestShift = shift;
+                }
+            }
+
+            state.ApplyShift(project, bestShift);
+            Console.WriteLine(project.name + " -> shift " + bestShift + ", conflicts " + bestConflicts);
+        }
+
+        int doubleBooked = state.PersonWeekGrid.Count(kv => kv.Value > 1);
+        Console.WriteLine("Done. Double-booked weeks: " + doubleBooked);
+    }
+
+    //Counts how many conflicts someone has
+    private int CountConflicts(ScheduleState state, Project project, int shift)
+    {
+        int conflicts = 0;
+        foreach (var (personId, week) in state.GetFootprintForShift(project, shift))
+        {
+            if (state.PersonWeekGrid.GetValueOrDefault((personId, week)) > 0)
+                conflicts++;
+        }
+        return conflicts;
+    }
+}
+
+//Since we designed it without a schedule class in the loader we keep track here
+public class ScheduleState
+{
+    private const int Weeks = 52;
+    public List<Person> People { get; } //list of all people in that schedule
+    public List<Project> Projects { get; } //list of projects
+    public Dictionary<(int personId, int week), int> PersonWeekGrid { get; } = new(); //dictionary to track projects a person has each week
+
+    private readonly Dictionary<Project, (int start, int end)> _window; //considers start and end dates to know what valid moves are 
+    private readonly Dictionary<Project, int> _shift; //current shift of project
+
+    public ScheduleState(List<Person> people, List<Project> projects)
+    {
+        People = people;
+        Projects = projects;
+        _window = projects.ToDictionary(p => p, p => (p.startDate, p.endDate));
+        _shift = projects.ToDictionary(p => p, _ => 0);
+        RebuildGrid();
+    }
+
+
+    //duration should be from start date week to end date week -1 but need to check my maths on this one on paper
+    public int GetDuration(Project p) => _window[p].end - _window[p].start + 1;
+    public int GetShift(Project p) => _shift[p];
+    public void SetShift(Project p, int shift) => _shift[p] = shift;
+
+    public List<int> GetValidShifts(Project p)
+    {
+
+        var weeks = p.people
+            .SelectMany(person => person.projects[p])
+            .Distinct()
+            .OrderBy(w => w)
+            .ToList();
+
+        if (weeks.Count == 0)
+        {
+            return new List<int> { 0 }; //Was getting an error for no movement allowed projects. I am not sure why this fixes it but please don't remove it
+        }
+        int baselineStart = weeks.First();
+        int duration = weeks.Count;
+
+        int earliestStart = _window[p].start + 1;
+        int latestEnd = _window[p].end - 1;
+
+        int minShift = earliestStart - baselineStart;
+        int maxShift = latestEnd - (baselineStart + duration - 1);
+
+        if (maxShift < minShift)
+        {
+            return new List<int>();
+        }
+        return Enumerable.Range(minShift, maxShift - minShift + 1).ToList();
+    }
+
+    public IEnumerable<(int personId, int week)> GetFootprintForShift(Project p, int shift)
+    {
+        foreach (var person in p.people)
+            for (int w = _window[p].start; w <= _window[p].end; w++)
+            {
+                yield return (person.id, w + shift);
+
+            }
+    }
+
+    public void RebuildGrid()
+    {
+        PersonWeekGrid.Clear();
+        foreach (var p in Projects)
+        {
+            AddProjectToGrid(p);
+
+        }
+    }
+
+    public void ApplyShift(Project p, int shift)
+    {
+        RemoveProjectFromGrid(p);
+        SetShift(p, shift);
+        AddProjectToGrid(p);
+    }
+
+    private void RemoveProjectFromGrid(Project p)
+    {
+        int shift = GetShift(p);
+        foreach (var (personId, week) in GetFootprintForShift(p, shift))
+        {
+            if (week is < 1 or > Weeks) continue;
+            var key = (personId, week);
+            if (!PersonWeekGrid.TryGetValue(key, out var count)) continue;
+            if (--count == 0) PersonWeekGrid.Remove(key);
+            else PersonWeekGrid[key] = count;
+        }
+    }
+
+    private void AddProjectToGrid(Project p)
+    {
+        int shift = GetShift(p);
+        foreach (var (personId, week) in GetFootprintForShift(p, shift))
+        {
+            if (week is < 1 or > Weeks) continue;
+            var key = (personId, week);
+            PersonWeekGrid[key] = PersonWeekGrid.GetValueOrDefault(key) + 1;
+        }
+    }
+}
--- CertavueApp/Program.cs
+++ CertavueApp/Program.cs
@@ -1,8 +1,9 @@
-﻿using static Project;
+﻿﻿using static Project;
 using static Person;
 using static Loader;
 using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.IO.Pipes;
 public class Program
 {
@@ -12,34 +13,46 @@ public class Program
     public Program()
     {
         loadData();
-        testPrint();
+        //testPrint();
+        var beforeGreedy = new ScheduleState(people, projects);
+        GreedyChecker("Before Greedy", beforeGreedy);
+        new GreedyAlg().StartGreedy(people, projects);
+        var afterGreedy = new ScheduleState(people, projects);
+        GreedyChecker("After Greedy", afterGreedy);
     }
 
     public void loadData()
     {
         Loader load = new Loader();
-        (this.people, this.projects) = load.LoadData("Data/schedule_target75_small.csv");
+        (this.people, this.projects) = load.LoadData("Data/schedule_target75_large.csv");
         Console.WriteLine("Loaded.");
     }
 
 
+    private void GreedyChecker(string label, ScheduleState state)
+    {
+        int doubleBooked = state.PersonWeekGrid.Count(kv => kv.Value > 1);
+        int total = state.PersonWeekGrid.Count;
+        double pctClear = total == 0 ? 100 : (double)(total - doubleBooked) / total * 100;
+        Console.WriteLine(label + " Double-booked= " + doubleBooked + " % not double-booked= " + pctClear);
+    }
     public void testPrint()
     {
-        Console.WriteLine("People:");
-        foreach (var p in people)
-        {
-            Console.WriteLine("- " + p.name);
-            foreach (KeyValuePair<Project, List<int>> kvp in p.projects)
-            {
-                List<int> values = kvp.Value;
-                foreach (var v in values)
-                {
-                    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key.name, v);
-                }
-            }
+        // Console.WriteLine("People:");
+        // foreach (var p in people)
+        // {
+        //     Console.WriteLine("- " + p.name);
+        //     foreach (KeyValuePair<Project, List<int>> kvp in p.projects)
+        //     {
+        //         List<int> values = kvp.Value;
+        //         foreach (var v in values)
+        //         {
+        //             Console.WriteLine("Key = {0}, Value = {1}", kvp.Key.name, v);
+        //         }
+        //     }
 
-        }
-        Console.WriteLine("Count of projects: " + projects.Count);
+        // }
+        // Console.WriteLine("Count of projects: " + projects.Count);
     }
     static void Main(string[] args)
     {
