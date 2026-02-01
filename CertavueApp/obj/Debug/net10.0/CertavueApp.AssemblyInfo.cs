078187e27512e2dbc397142899fe185f7f96c61c
Kukuwai <millar.sykes@gmail.com>
2026-02-02 11:41:39 +1300

    nothing done

--- CertavueApp/obj/Debug/net10.0/CertavueApp.AssemblyInfo.cs
+++ CertavueApp/obj/Debug/net10.0/CertavueApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CertavueApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9020220a8aa4f39c923b695d54801e35051e3ed5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ffe22704ec59b79339260130e4144a206a283a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CertavueApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CertavueApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
