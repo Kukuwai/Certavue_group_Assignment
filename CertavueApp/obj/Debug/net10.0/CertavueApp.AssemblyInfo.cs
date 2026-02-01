aa2bd231b2f47aefd01610cd2c7b905a51201f30
milla <millar.sykes@gmail.com>
2026-02-01 23:24:22 +1300

    adding missing files to run

--- CertavueApp/obj/Debug/net10.0/CertavueApp.AssemblyInfo.cs
+++ CertavueApp/obj/Debug/net10.0/CertavueApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CertavueApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ccbece8400f2be46d96dc0a92195e3a161f0924")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+596fbfc21a0b212da0ace39f60ffdf4e0c258873")]
 [assembly: System.Reflection.AssemblyProductAttribute("CertavueApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CertavueApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
