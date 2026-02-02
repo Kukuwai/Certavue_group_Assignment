ffcc0346e7bb0b74de48ca5b7b068eecb34e282f
milla <millar.sykes@gmail.com>
2026-02-02 21:35:52 +1300

    new getShift method for getting available/valid shifts within the state passed into the algo

--- CertavueApp/MoveByConflict.cs
+++ CertavueApp/MoveByConflict.cs
@@ -3,6 +3,8 @@ using System.Data;
 using static Program;
 using static Person;
 using static GreedyAlg;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.CompilerServices;
 
 public class MoveByConflict
 {
@@ -10,6 +12,9 @@ public class MoveByConflict
 
     public MoveByConflict(ScheduleState state, List<Project> projects)
     {
+        // reference score
+        var scorer = new GreedyAlg();
+
         var ProjectsWithConflict = new Dictionary<Project, int>();
         foreach (KeyValuePair<ScheduleState.WeekKey, int> entry in state.PersonWeekGrid)
         {
@@ -35,8 +40,8 @@ public class MoveByConflict
 
         var ProjectsSortedByConflict = ProjectsWithConflict.OrderByDescending(pair => pair.Value);
 
-        // foreach (var sort in ProjectsSortedByConflict)
-        // {
+        
+        // foreach (var sort in ProjectsSortedByConflict) 
         //     Console.WriteLine(sort.Key.name + " " + sort.Value);
         // }
 
@@ -63,13 +68,25 @@ public class MoveByConflict
 
         Console.WriteLine("Start total: " + startTotal + ", double-booked=" + startDouble + ", % not double-booked=" + startPct.ToString("0.##"));
 
+        bool shifted = false;
 
         foreach (var project in ProjectsSortedByConflict)
         {
             //some helpful methods
-            //getvalidshifts in schedulestate, can't call because we will have +/- 3 logic but can copy and paste into this class and refactor
+            
             //get and set shift should be recyclable
+
+            // get current shift in the schedule state (from greedy)
+            int currentShift = state.GetShift(project.Key);
+            int bestShift = currentShift;
+
             //Evaluate shift does the scoring of overlaps. 
+
+            ShiftScore best = scorer.EvaluateShift(state, project.Key, currentShift);
+
+            //getvalidshifts in schedulestate, can't call because we will have +/- 3 logic but can copy and paste into this class and refactor
+
+            var potentialShifts = getSetShifts(state....project, 3)
             //Get grid handles some of the removing and placing logic
             //build greedy schedule lines 46-73 has the bulk of the actual greedy logic so it can be repurposed and moved here
             //apply shift actually sets the shift when we confirm the best move
@@ -77,7 +94,21 @@ public class MoveByConflict
         }
 
 
-
+        private List<int> getSetShifts(ScheduleState state, Project project, int maxShift)
+        {
+            int currentShift = state.GetShift(project);
+            List<int> shifts = new List<int>();
+            foreach (var shift in state.GetValidShifts(project))
+            {
+               // check shifts are valid in either direction (-/+)
+               if (Math.Abs(shift - currentShift) <= maxShift)
+                {
+                    // add if within maxShift 
+                    shifts.Add(shift);
+                }
+            }
+            return shifts;
+        }
 
     }
 }
