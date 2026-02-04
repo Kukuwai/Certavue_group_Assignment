2c337d33109c6b0b378028a32376a2931a88f465
ColyanM <colyanmiller@yahoo.com>
2026-02-04 13:56:41 +1300

    Constructors

--- CertavueApp/Person.cs
+++ CertavueApp/Person.cs
@@ -7,12 +7,14 @@ public class Person
     public string name {get; set;}
     public Dictionary<Project, List<int>> projects { get; } = new();
     public int capacity { get; set;}
+    public string role { get; set;}
 
-    public Person(string name, int capacity)
+    public Person(string name, int capacity, string role)
     {
         id = ++idCounter;
         this.name = name;
         this.capacity = capacity;
+        this.role = role;
     }
 
 }
